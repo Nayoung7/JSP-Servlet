5afde4039aec43bdcf6d9a220abb2e501c4afe26
Nayoung7 <95176265+Nayoung7@users.noreply.github.com>
2022-01-17 21:49:13 +0900

    MessageSystem - Model

--- model/MessageDAO.java
+++ model/MessageDAO.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MessageDAO {
-	
+
 	private Connection conn;
 	private PreparedStatement psmt;
 	private ResultSet rs;
 	private int cnt;
 	private String sql;
-	
+
 	public void connect() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -51,29 +52,58 @@ public class MessageDAO {
 		}
 
 	}
-	
+
 	// 전송한 메세지 저장하는 기능
-	public int messageSend(MessageDTO message) {
-		
+	public int sendMessage(MessageDTO message) {
+
 		connect();
-		
+
 		sql = "insert into web_message values(num_seq.nextval,?,?,?,sysdate)";
-		
+
 		try {
 			psmt = conn.prepareStatement(sql);
 			psmt.setString(1, message.getM_sendName());
 			psmt.setString(2, message.getM_receiveEmail());
 			psmt.setString(3, message.getM_content());
-			
+
 			cnt = psmt.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			close();
 		}
-		
-		close();
-		
 		return cnt;
+	}
+
+	public ArrayList<MessageDTO> receiveMessage(String email) {
+
+		connect();
+		ArrayList<MessageDTO> message = new ArrayList<MessageDTO>();
+
+		sql = "select m_num, m_sendName, m_content, m_sendDate from web_message where m_receiveEmail = ?";
+
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, email);
+
+			rs = psmt.executeQuery();
+
+			while (rs.next()) {
+				
+				int num = rs.getInt("m_num");
+				String sendName = rs.getString("m_sendName");
+				String content = rs.getString("m_content");
+				String sendDate = rs.getString("m_sendDate");
+				message.add(new MessageDTO(num, sendName, email, content, sendDate));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		return message;
 	}
 
 }
